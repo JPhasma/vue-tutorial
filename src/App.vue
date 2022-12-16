b97379134598a4bbaf7338b30f82c68368393c01
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-12-16 22:40:19 +0000

    vue basics and how to use type assertion

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="app">HI</div>
+  <div class="app">
+    <h1>HI</h1>
+    <p>{{ name }}</p>
+    <button @click="changeName('Jackie Daytona')">change name</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-export default {};
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Laslo Cravensworth',
+      age: 180 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
+});
 </script>
 
 <style></style>
